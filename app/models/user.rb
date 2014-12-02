0199e92e8e319548f994c879053ce689a9401176
vagrant <vagrant@rails-dev-box>
2014-12-02 21:13:08 +0000

    Fixed leftover merge conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,14 +20,14 @@
 #
 
 class User < ActiveRecord::Base
-  has_many :dogs
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  #validates :name, presence: {:message => "No name given"}
-  #validates :name, uniqueness: {:message => "Already exists"}
-  
+  validates :name, presence: {:message => "No name given"}
+  validates :name, uniqueness: {:message => "Already exists"}
+
+  has_many :dogs
   has_many :messages
   has_many :reviews
   belongs_to :cities
